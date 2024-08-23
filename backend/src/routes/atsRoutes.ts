f085f6cc656a3c1f5cf8d25b1cd6dddfde607de7
anuragkainth <anuragkainth07@gmail.com>
2024-08-23 12:24:36 +0530

    feat: candidate view backend added

--- backend/src/routes/atsRoutes.ts
+++ backend/src/routes/atsRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { listAllApplications, listAllJobs } from '../service/atsService';
+import { createApplication, listAllApplications, listAllJobs, listPostedJobs } from '../service/atsService';
 import { Request, Response } from 'express';
 import { handleErrorResponse } from './routesErrorHandler';
 
@@ -30,5 +30,31 @@ router.get('/applications', async (req: Request, res: Response) => {
         handleErrorResponse(error, res); 
     }
 });
+
+router.get('/job-postings', async (req: Request, res: Response) => {
+    const { query, headers } = req;
+    const next: string = query.next as string;
+    const accountId: string = headers['x-account-id'] as string;
+  
+    try {
+      const postedJobs = await listPostedJobs(accountId, next);
+      res.status(200).send(postedJobs);
+    } catch (error: unknown) {
+      handleErrorResponse(error, res); 
+    }
+  });
+  
+  router.post('/applications', async (req: Request, res: Response) => {
+    const { headers, body } = req;
+    const accountId: string = headers['x-account-id'] as string;
+    const applicationData = body;
+  
+    try {
+      const newApplication = await createApplication(accountId, applicationData);
+      res.status(201).send(newApplication);
+    } catch (error: unknown) {
+      handleErrorResponse(error, res); 
+    }
+  });
 
 export default router;
--- backend/src/service/atsService.ts
+++ backend/src/service/atsService.ts
@@ -1,4 +1,4 @@
-import { getJobs , getApplications } from '../http/stackOneAts';
+import { getJobs , getApplications, getPostedJobs, postApplication } from '../http/stackOneAts';
 
 export const listAllJobs = async (accountId: string, next: string) => {
   return await getJobs(accountId, next);
@@ -6,4 +6,13 @@ export const listAllJobs = async (accountId: string, next: string) => {
 
 export const listAllApplications = async (accountId: string, next: string) => {
   return await getApplications(accountId, next);
+};
+
+export const listPostedJobs = async (accountId: string, next: string) => {
+  return await getPostedJobs(accountId, next);
+};
+
+
+export const createApplication = async (accountId: string, applicationData: unknown) => {
+  return await postApplication(accountId, applicationData);
 };
